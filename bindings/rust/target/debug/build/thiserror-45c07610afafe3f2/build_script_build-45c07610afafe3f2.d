2d4c8c08acee974734cc2558b2c5f2c909e292ce
Zach Kelling <z@zeekay.io>
2025-12-27 15:10:11 -0800

    chore: sync local changes

---
+++ bindings/rust/target/debug/build/thiserror-45c07610afafe3f2/build_script_build-45c07610afafe3f2.d
@@ -0,0 +1,5 @@
+/Users/z/work/lux/crypto/bindings/rust/target/debug/build/thiserror-45c07610afafe3f2/build_script_build-45c07610afafe3f2.d: /Users/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/build.rs
+
+/Users/z/work/lux/crypto/bindings/rust/target/debug/build/thiserror-45c07610afafe3f2/build_script_build-45c07610afafe3f2: /Users/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/build.rs
+
+/Users/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.18/build.rs:
