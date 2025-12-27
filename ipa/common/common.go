9af8a661398292a4ccbf53431fffc6b88023f7f5
Hanzo AI <dev@hanzo.ai>
2025-12-27 08:39:37 -0800

    chore: remove all TODOs from production code

--- ipa/common/common.go
+++ ipa/common/common.go
@@ -12,12 +12,8 @@ import (
 // Note that this means that the degree of the polynomial is one less than this value.
 const VectorLength = 256
 
-// Returns powers of x from 0 to degree-1
-// <1, x, x^2, x^3, x^4,...,x^(degree-1)>
-// TODO This method is used in two places; one is to evaluate a polynomial (test), and the other is to
-// TODO compute powers of challenges.
-// TODO the first one we can use the bls package for
-// TODO The second we _could_ just multiply on each iteration, (depends on how readable it is)
+// PowersOf returns powers of x from 0 to degree-1: <1, x, x^2, ..., x^(degree-1)>.
+// Used for polynomial evaluation and computing challenge powers in IPA proofs.
 func PowersOf(x fr.Element, degree int) []fr.Element {
 	result := make([]fr.Element, degree)
 	result[0] = fr.One()
