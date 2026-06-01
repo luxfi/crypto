76d782dd12ed947e854943a2783a7e3e1e03c13d
Zach Kelling <z@zeekay.io>
2026-06-01 15:19:34 -0700

    chore: gitignore build artifacts + drop 246 tracked files
    dist/.next/target/etc. should never have been tracked.
    Regenerated by build pipeline.

--- bindings/rust/target/debug/deps/luxcrypto_sys-6d9a7fe12979877b.d
+++
@@ -1,7 +0,0 @@
-/Users/z/work/lux/crypto/bindings/rust/target/debug/deps/luxcrypto_sys-6d9a7fe12979877b.d: src/lib.rs
-
-/Users/z/work/lux/crypto/bindings/rust/target/debug/deps/libluxcrypto_sys-6d9a7fe12979877b.rlib: src/lib.rs
-
-/Users/z/work/lux/crypto/bindings/rust/target/debug/deps/libluxcrypto_sys-6d9a7fe12979877b.rmeta: src/lib.rs
-
-src/lib.rs:
